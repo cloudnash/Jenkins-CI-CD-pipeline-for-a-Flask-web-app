217558f469fc12d33fc4fefdfd8cab2ee6bd9d8c
cloudnash <fusionmad47@gmail.com>
2025-10-17 19:58:43 +0530

    Fix greeting message formatting in home route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello — deployed via Jenkins CI/CD!"
+    return "Hello  deployed via Jenkins CI/CD!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
